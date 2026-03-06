d42c6bd47b5bc17b6e183bd217cd5a0693d945c8
yididiya1 <yididiyakebede@gmail.com>
2026-03-06 03:19:20 -0500

    feat: add Radix Tooltip component and GradientSparklesIcon

---
+++ src/components/icons/GradientSparklesIcon.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { useId } from 'react'
+
+type Props = {
+  className?: string
+  size?: number
+  strokeWidth?: number
+}
+
+export function GradientSparklesIcon({ className, size = 16, strokeWidth = 2 }: Props) {
+  const id = useId()
+  const gradientId = `sparklesGradient-${id}`
+
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke={`url(#${gradientId})`}
+      strokeWidth={strokeWidth}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={className}
+      aria-hidden="true"
+      focusable="false"
+    >
+      <defs>
+        <linearGradient id={gradientId} x1="0%" y1="0%" x2="100%" y2="0%">
+          <stop offset="0%" stopColor="#40c9ff" />
+          <stop offset="100%" stopColor="#e81cff" />
+        </linearGradient>
+      </defs>
+
+      {/* Lucide Sparkles icon paths */}
+      <path d="M11.017 2.814a1 1 0 0 1 1.966 0l1.051 5.558a2 2 0 0 0 1.594 1.594l5.558 1.051a1 1 0 0 1 0 1.966l-5.558 1.051a2 2 0 0 0-1.594 1.594l-1.051 5.558a1 1 0 0 1-1.966 0l-1.051-5.558a2 2 0 0 0-1.594-1.594l-5.558-1.051a1 1 0 0 1 0-1.966l5.558-1.051a2 2 0 0 0 1.594-1.594z" />
+      <path d="M20 2v4" />
+      <path d="M22 4h-4" />
+      <circle cx="4" cy="20" r="2" />
+    </svg>
+  )
+}
